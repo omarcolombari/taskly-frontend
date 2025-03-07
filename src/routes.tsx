f447aad089bef8bd742d34023fb2c79cb1e60bd5
Omar Colombari <omarucolombari@gmail.com>
2025-03-06 23:27:18 -0300

    feat: create routes

---
+++ src/routes.tsx
@@ -0,0 +1,33 @@
+import { createBrowserRouter } from 'react-router'
+import Dashboard from './pages/app/dashboard'
+import { SignIn } from './pages/auth/sign-in'
+import { SignUp } from './pages/auth/sign-up'
+import { AppLayout } from './pages/_layout/app'
+import { AuthLayout } from './pages/_layout/auth'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Dashboard />,
+      },
+    ],
+  },
+  {
+    path: '/',
+    element: <AuthLayout />,
+    children: [
+      {
+        path: '/sign-in',
+        element: <SignIn />,
+      },
+      {
+        path: '/sign-up',
+        element: <SignUp />,
+      },
+    ],
+  },
+])
