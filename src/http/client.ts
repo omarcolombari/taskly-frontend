6647ac0e9699c9d3b96d4a9e732f048f93a1ae61
Omar Colombari <omarucolombari@gmail.com>
2025-03-07 02:22:27 -0300

    refactor: refactor client http with axios

--- src/http/client.ts
+++ src/http/client.ts
@@ -1,43 +1,73 @@
 import { TOKEN_COOKIE_KEY } from '@/auth/auth'
 import { env } from '@/env'
 import Cookies from 'universal-cookie'
+import axios, { type AxiosRequestConfig, type AxiosResponse } from 'axios'
 
-async function getHeaders(headers?: HeadersInit): Promise<HeadersInit> {
+async function getHeaders(
+  headers?: HeadersInit
+): Promise<Record<string, string>> {
   const cookies = new Cookies()
   const token = cookies.get(TOKEN_COOKIE_KEY)
 
-  if (token) {
-    return { ...headers, Authorization: `Bearer ${token}` }
+  const headersObject: Record<string, string> = {}
+
+  if (headers) {
+    if (headers instanceof Headers) {
+      headers.forEach((value, key) => {
+        headersObject[key] = value
+      })
+    } else if (Array.isArray(headers)) {
+      headers.forEach(([key, value]) => {
+        headersObject[key] = value
+      })
+    } else {
+      Object.assign(headersObject, headers)
+    }
   }
 
-  return headers ?? {}
+  if (token) {
+    headersObject['Authorization'] = `Bearer ${token}`
+  }
+
+  return headersObject
 }
+
+export const api = axios.create({
+  baseURL: env.VITE_API_URL,
+})
 
 export async function http<T>(path: string, options: RequestInit): Promise<T> {
   const headers = await getHeaders(options.headers)
 
-  const url = new URL(path, env.VITE_API_URL)
-
-  const request = new Request(url, {
-    ...options,
+  const axiosConfig: AxiosRequestConfig = {
+    method: options.method,
     headers,
-  })
-
-  const response = await fetch(request)
-
-  await new Promise(resolve => setTimeout(resolve, 2000))
-
-  if (response.ok) {
-    if (response.headers.get('content-type')?.includes('application/json')) {
-      const data = await response.json()
-
-      return data as T
-    }
-
-    const data = response.text()
-
-    return data as T
+    data: options.body,
+    url: path,
+    baseURL: env.VITE_API_URL,
   }
 
-  return Promise.reject(response)
+  await new Promise(resolve => setTimeout(resolve, 2000))
+  const response: AxiosResponse = await api.request(axiosConfig)
+
+  if (response.status >= 200 && response.status < 300) {
+    const contentType = response.headers['content-type']
+    if (contentType?.includes('application/json')) {
+      return response.data as T
+    }
+    return response.data as T
+  }
+
+  return Promise.reject({
+    ok: false,
+    status: response.status,
+    statusText: response.statusText,
+    json: () => Promise.resolve(response.data),
+    text: () =>
+      Promise.resolve(
+        typeof response.data === 'string'
+          ? response.data
+          : JSON.stringify(response.data)
+      ),
+  })
 }
